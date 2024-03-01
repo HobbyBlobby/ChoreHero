f8e25bc9bf71b4f0f19f1b2b00cce14485035103
Felix Lemke <fl@informatik-dv.com>
2024-03-01 16:29:11 +0100

    solved double router-outlet error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,28 @@
-import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { GroupsComponent } from '../groups/groups.component';
+import { Component, ViewChild } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from  '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 import {HttpClientModule} from '@angular/common/http';
+import {MatListModule} from '@angular/material/list'
+import {MatSidenavModule, MatSidenav} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GroupsComponent, MatToolbarModule, HttpClientModule],
+  imports: [RouterModule, MatToolbarModule, HttpClientModule, MatIconModule, MatButtonModule, MatSidenavModule, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-test';
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
 
-  constructor() {
+  constructor() {}
+
+  toggleMenu(): void {
+    console.log("clicked");
+    this.sidenav.toggle();
   }
 
 }
