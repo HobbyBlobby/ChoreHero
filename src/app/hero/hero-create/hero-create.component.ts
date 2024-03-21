354798035c6bb59a42a6baa7cdfafc8ec6275cb3
HobbyBlobby <flemke2go@gmail.com>
2024-03-21 15:45:51 +0100

    carousel (primeng) with hero class selection

--- src/app/hero/hero-create/hero-create.component.ts
+++ src/app/hero/hero-create/hero-create.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
+import {CarouselModule } from 'primeng/carousel';
+import {MatButtonModule} from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
   imports: [
+    CarouselModule,
+    MatButtonModule
   ],
   templateUrl: './hero-create.component.html',
   styleUrl: './hero-create.component.scss'
 })
 export class HeroCreateComponent {
+  heroClasses = [{
+    class_name: "Fortuneteller",
+    img: "/assets/heros/fortuneteller_upperbody.png" 
+  },{
+    class_name: "IT-Pro",
+    img: "/assets/heros/itpro_upperbody.png" 
+  },{
+    class_name: "Gunner",
+    img: "/assets/heros/gunner_upperbody.png" 
+  }];
 
+  constructor(private snackBar: MatSnackBar) {}
+  selectClass(class_name: string): void {
+    this.snackBar.open('Du hast dich für ' + class_name + ' entschieden.', undefined,
+    { duration: 3000, panelClass: ['snack_bar'] });
+  }
 }
