acac7dd6d0a138cb7cc83998fb9db18b72e564fb
HobbyBlobby <flemke2go@gmail.com>
2024-03-21 16:10:56 +0100

    forward navigation after hero creation (but actual creation is missing^^)

--- src/app/hero/hero-create/hero-create.component.ts
+++ src/app/hero/hero-create/hero-create.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {CarouselModule } from 'primeng/carousel';
 import {MatButtonModule} from '@angular/material/button';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
@@ -14,6 +15,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrl: './hero-create.component.scss'
 })
 export class HeroCreateComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  group_id: number = -1;
   heroClasses = [{
     class_name: "Fortuneteller",
     img: "/assets/heros/fortuneteller_upperbody.png" 
@@ -25,9 +28,14 @@ export class HeroCreateComponent {
     img: "/assets/heros/gunner_upperbody.png" 
   }];
 
-  constructor(private snackBar: MatSnackBar) {}
+  constructor(
+    private snackBar: MatSnackBar,
+    private router: Router) {
+      this.group_id = this.route.snapshot.params['group_id'];
+    }
   selectClass(class_name: string): void {
     this.snackBar.open('Du hast dich für ' + class_name + ' entschieden.', undefined,
     { duration: 3000, panelClass: ['snack_bar'] });
+    this.router.navigate(["/groupDetails", this.group_id]);
   }
 }
