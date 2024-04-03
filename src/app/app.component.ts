d992076b3e457abbd090f9bbdc8a79e22ae9d136
HobbyBlobby <flemke2go@gmail.com>
2024-04-03 12:08:42 +0200

    further transition to primeng+primeflex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component, Inject, ViewChild } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
+// import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from  '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+// import {MatButtonModule} from '@angular/material/button';
 import {HttpClientModule} from '@angular/common/http';
 import {MatListModule} from '@angular/material/list'
-import {MatSidenavModule, MatSidenav} from '@angular/material/sidenav';
+// import {MatSidenavModule, MatSidenav} from '@angular/material/sidenav';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LoginService } from '../login/login.service';
@@ -15,18 +15,30 @@ import { Observable } from 'rxjs';
 import { bottomAction } from './interfaces';
 
 
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { DockModule } from 'primeng/dock';
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
     RouterModule, 
-    MatToolbarModule, 
+    // MatToolbarModule, 
     HttpClientModule, 
     MatIconModule, 
-    MatButtonModule, 
-    MatSidenavModule, 
-    MatListModule
+    // MatButtonModule, 
+    // MatSidenavModule, 
+    MatListModule,
+    SidebarModule,
+    ButtonModule,
+    ToolbarModule,
+    DockModule,
+    MenuModule
     ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -34,10 +46,12 @@ import { bottomAction } from './interfaces';
 })
 export class AppComponent {
   title = 'angular-test';
-  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+  // @ViewChild(MatSidenav) sidenav!: MatSidenav;
   public menuEntries : Observable<bottomAction[]>;
   public bottomActions : bottomAction[];
-
+  public sidebarVisible : boolean = false;
+  mainMenu: MenuItem[] | undefined;
+  
   constructor(
     private snackBar: MatSnackBar,
     private activatedRoute: ActivatedRoute, 
@@ -49,13 +63,31 @@ export class AppComponent {
   this.bottomActions = [];
   this.menuEntries = this.appService.changeOnActions$; //.subscribe(value => {this.menuEntries = value;});
   this.menuEntries.subscribe(actions => this.bottomActions = actions || []);
+
+  window.onscroll = function() {
+    console.log("Scroll");
+    document.getElementsByClassName("top-toolbar").item(0)?.classList.toggle("top-toolbar-blur", document.documentElement.scrollTop > 50);
+    // document.getElementsByClassName("top-toolbar").item(0)?.classList.add("myToolbar")
+  };
 }
 
   noOnInit(): void {
-  }
+    this.mainMenu = [{
+          label: 'Account',
+          items: [
+              {
+                  label: 'Logout',
+                  icon: 'pi pi-arrow-left',
+                  command: () => {
+                      this.submitLogout();
+                  }
+              }
+          ]
+      }];
+    }
 
   toggleMenu(): void {
-    this.sidenav.toggle();
+    // this.sidenav.toggle();
   }
   navBack(): void {
     this.location.back();
@@ -70,7 +102,7 @@ export class AppComponent {
       next: res => {
         localStorage.clear();
         this.snackBar.open('Auf wiedersehen ' + res.data.account_name + "!", undefined, {duration: 3000});
-        this.sidenav.close();
+        this.sidebarVisible = false;
         this.router.navigate([''], {relativeTo: this.activatedRoute});
       },
       error: err => this.loginService.handleServerError(err)
